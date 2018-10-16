90073f04e4cc2fe5b1e2fd6cc331e825939aa2ae
zhangjincheng <zhangjincheng@huidr.com>
2018-10-16 10:18:46 +0800

    update

--- src/main/java/com/huiyi/plugin/tools/JavaTool.java
+++ src/main/java/com/huiyi/plugin/tools/JavaTool.java
@@ -16,24 +16,34 @@ public class JavaTool extends BaseTools {
     }
 
     public boolean isBaseType(String s) {
-        if(s.equals("T")) return true;
+        if (s.equals("T")) return true;
         String type = baseType(s);
-        if(baseType.containsValue(type)){
+        if (baseType.containsValue(type)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    //是否是泛型
+    public boolean isT(String s) {
+        int startA = s.indexOf("{");
+        if (startA > 0) {
+            return true;
+        }
+        return false;
+    }
+
     //类型转换
     public String typeChange(String s) {
         int startA = s.indexOf("{");
         int startB = s.lastIndexOf("}");
-        if(startA>0){
-            String change = typeChange(s.substring(startA+1,startB));
-            s = s.substring(0,startA+1)+change+s.substring(startB);
+        if (startA > 0) {
+            String change = typeChange(s.substring(startA + 1, startB));
+            s = s.substring(0, startA + 1) + change + s.substring(startB);
         }
-        s = s.replaceAll("\\{","<");
-        s = s.replaceAll("\\}",">");
+        s = s.replaceAll("\\{", "<");
+        s = s.replaceAll("\\}", ">");
         int arrIndex = s.indexOf("[]");
         boolean isArray = false;
         String result = "";
@@ -52,10 +62,11 @@ public class JavaTool extends BaseTools {
             return result;
         }
     }
+
     //基础类型
-    public String baseType(String s){
-        s = s.replaceAll("\\{","<");
-        s = s.replaceAll("\\}",">");
+    public String baseType(String s) {
+        s = s.replaceAll("\\{", "<");
+        s = s.replaceAll("\\}", ">");
         int arrIndex = s.indexOf("[]");
         String result;
         if (arrIndex > 0) {
@@ -68,6 +79,7 @@ public class JavaTool extends BaseTools {
         }
         return result;
     }
+
     public String getTypeClass(String s) {
         int arrIndex = s.indexOf("[]");
         if (arrIndex > 0) {
@@ -75,14 +87,14 @@ public class JavaTool extends BaseTools {
         } else {
             arrIndex = s.indexOf("{");
             if (arrIndex > 0) {
-                s = s.substring(0,arrIndex);
+                s = s.substring(0, arrIndex);
             }
             return toUpperCaseFirstOne(s) + ".class";
         }
     }
 
-    public String getPath(String packageName){
-        return packageName.replaceAll("\\.","/")+"/";
+    public String getPath(String packageName) {
+        return packageName.replaceAll("\\.", "/") + "/";
     }
 
 }
