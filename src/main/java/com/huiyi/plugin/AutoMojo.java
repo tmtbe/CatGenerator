841f149b131e5e484b8e46c1960cc693be2ab9a0
zhangjincheng <zhangjincheng@huidr.com>
2018-10-18 14:45:55 +0800

    update

--- src/main/java/com/huiyi/plugin/AutoMojo.java
+++ src/main/java/com/huiyi/plugin/AutoMojo.java
@@ -30,7 +30,8 @@ public class AutoMojo extends AbstractMojo {
 
     public void execute() throws MojoExecutionException {
         Build build = project.getBuild();
-
+        String autoGenpath = build.getSourceDirectory() + "/com/huiyi/autogen";
+        deleteDirectory(autoGenpath);
         Global.LocalPath = build.getSourceDirectory() + "/";
         String filepath = build.getSourceDirectory() + "/../resources/catgen/";
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
@@ -46,6 +47,51 @@ public class AutoMojo extends AbstractMojo {
         run(filepath, "rabbit");
     }
 
+    /**
+     * 删除单个文件
+     * @param   sPath    被删除文件的文件名
+     * @return 单个文件删除成功返回true，否则返回false
+     */
+    public void deleteFile(String sPath) {
+        File file = new File(sPath);
+        // 路径为文件且不为空则进行删除
+        if (file.isFile() && file.exists()) {
+            file.delete();
+        }
+    }
+    /**
+     * 删除目录（文件夹）以及目录下的文件
+     * @param   sPath 被删除目录的文件路径
+     * @return  目录删除成功返回true，否则返回false
+     */
+    public boolean deleteDirectory(String sPath) {
+        //如果sPath不以文件分隔符结尾，自动添加文件分隔符
+        if (!sPath.endsWith(File.separator)) {
+            sPath = sPath + File.separator;
+        }
+        File dirFile = new File(sPath);
+        //如果dir对应的文件不存在，或者不是一个目录，则退出
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            return false;
+        }
+        //删除文件夹下的所有文件(包括子目录)
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            //删除子文件
+            if (files[i].isFile()) {
+                deleteFile(files[i].getAbsolutePath());
+            } //删除子目录
+            else {
+                deleteDirectory(files[i].getAbsolutePath());
+            }
+        }
+        //删除当前目录
+        if (dirFile.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     private void run(String filepath, String environmentName) throws MojoExecutionException {
         ReadProto readProto = new ReadProto();
         File file = new File(filepath);
